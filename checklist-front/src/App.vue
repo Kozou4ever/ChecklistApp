1992e3525a5cb7faedc332925c6e71955e81ee52
Kozou <louisfloquey@gmail.com>
2023-01-12 20:03:06 +0100

    feat: font init commit

---
+++ checklist-front/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <h1 class="font-bold text-xl">Hello</h1>
+  </div>
+</template>
+
+
