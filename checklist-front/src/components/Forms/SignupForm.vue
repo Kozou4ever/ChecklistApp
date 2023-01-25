22fdc91c32f2a65b786dfca67d84144c78d8f5b0
Kozou <louisfloquey@gmail.com>
2023-01-25 22:01:19 +0100

    refactor: remove useless style

--- checklist-front/src/components/Forms/SignupForm.vue
+++ checklist-front/src/components/Forms/SignupForm.vue
@@ -1,13 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import AuthService from "@/services/AuthService";
+import Auth from '@/types/auth/Auth';
+import AuthResponse from '@/types/auth/AuthResponse';
+import { useAuthStore } from '@/store/authStore';
+import { storeToRefs } from 'pinia';
+
+const User = ref('');
+</script>
+
 <template>
     <form class="p-24 ">
         <div class="mb-6">
-            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ton pseudo</label>
-            <input type="email" id="email"
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Ton pseudo</label>
+            <input type="email" id="email" v-model="User"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                 placeholder="Kathy..." required>
         </div>
         <div class="mb-6">
-            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ton mot de
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Ton mot de
                 passe</label>
             <input type="password" id="password"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
--- checklist-front/src/components/Modals/ChecklistCreateModal.vue
+++ checklist-front/src/components/Modals/ChecklistCreateModal.vue
@@ -9,12 +9,13 @@ const { toggleChecklistCreateModal } = store;
 </script>
 
 <template>
-    <div id="defaultModal" tabindex="-1" aria-hidden="true" 
-        class="fixed grid h-screen place-items-center z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full" v-if="checklistCreate">
+    <div id="defaultModal" tabindex="-1" aria-hidden="true"
+        class="fixed grid h-screen place-items-center z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full"
+        v-if="checklistCreate">
         <div class="relative w-full h-full max-w-2xl md:h-auto">
-            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+            <div class="relative bg-white rounded-lg shadow">
+                <div class="flex items-start justify-between p-4 border-b rounded-t">
+                    <h3 class="text-xl font-semibold text-gray-900">
                         Créer une nouvelle liste de TODO
                     </h3>
                     <button type="button" @click="toggleChecklistCreateModal()"
@@ -26,17 +27,17 @@ const { toggleChecklistCreateModal } = store;
                 </div>
                 <div class="p-6 space-y-6">
                     <div class="mb-6">
-                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom :</label>
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Nom :</label>
                         <input type="email" id="email"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                             placeholder="Liste des courses à faire..." required>
                     </div>
                     <div class="flex items-center justify-end space-x-2">
                         <button data-modal-hide="defaultModal" type="submit"
-                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Créer</button>
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Créer</button>
                     </div>
                 </div>
-                
+
             </div>
         </div>
     </div>
