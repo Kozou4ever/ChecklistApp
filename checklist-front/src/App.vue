08d79e6526b00eed0a88e977e9ecf9d8af5ccd2c
kozou4ever <contact@kozou.dev>
2023-01-25 10:08:53 +0100

    refactor: App.vue

--- checklist-front/src/App.vue
+++ checklist-front/src/App.vue
@@ -1,43 +1,16 @@
 <script setup lang="ts">
-import AddButton from './components/Buttons/AddButton.vue';
-import CheckTodo from './components/Checkbox/CheckTodo.vue';
-import ChecklistButton from './components/Buttons/ChecklistButton.vue';
-import ChecklistCreateModal from './components/Modals/ChecklistCreateModal.vue';
-import TodoCreateModal from './components/Modals/TodoCreateModal.vue';
-
-import { useModalStore } from './store/modalStore';
+import AuthentificationView from './views/AuthentificationView.vue';
+import MainView from './views/MainView.vue';
+import { useAuthStore } from './store/authStore';
 import { storeToRefs } from 'pinia';
 
-const store = useModalStore();
-const { checklistCreate, todoCreate } = storeToRefs(store);
-const { toggleChecklistCreateModal, toggleTodoCreateModal } = store;
-
+const store = useAuthStore();
+const { isLogged } = storeToRefs(store);
 </script>
 
 <template>
-  <div class="min-h-screen flex" :class="checklistCreate || todoCreate ? 'blur' : ''">
-    <div class="w-56 flex flex-col border-r border-black p-4">
-      <AddButton @click="toggleChecklistCreateModal()" class="self-end" />
-      <div class="space-y-2 flex flex-col mt-6">
-        <ChecklistButton name="Checklist1" />
-        <ChecklistButton name="Checklist1" />
-        <ChecklistButton name="Checklist1" />
-        <ChecklistButton name="Checklist1" />
-      </div>
-    </div>
-  
-    <main class="flex-1 min-w-0 p-12">
-      <div class="flex flex-row justify-between">
-        <h1 class="font-bold text-6xl mb-12 break-all max-w-xl">Liste des courses </h1>
-        <AddButton class="ml-5" @click="toggleTodoCreateModal()" />
-      </div>
-      
-      <CheckTodo />
-      
-    </main>
+  <div>
+    <MainView v-if="isLogged" />
+    <AuthentificationView v-else />
   </div>
-  <ChecklistCreateModal />
-  <TodoCreateModal />
 </template>
-
-
